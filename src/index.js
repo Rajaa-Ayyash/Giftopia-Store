6f4ffa519dd7f9485453154080bc8360bf84e08c
Noorhan <s12115440@stu.najah.edu>
2024-04-04 20:17:22 +0300

    Add rating and comment

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import {
 
 
 import Home from './pages/home/Home.js';
+import Rating from './pages/rating/Rating.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
     path: "/home",
     element: <Home />,
   },
+  {
+    path: "/rating",
+    element: <Rating/>
+  }
 ]);
 root.render(
   <React.StrictMode>
---
+++ src/pages/rating/Rating.js
@@ -0,0 +1,30 @@
+import { Input, Modal, Rate } from 'antd';
+import React, { useState } from 'react';
+
+export const Rating = () => {
+
+  const [modal2Open, setModal2Open] = useState(true);
+
+  const onCommentChange = (e) => {
+    console.log('Change:', e.target.value);
+  };
+
+  return (
+    <div>
+      <Modal
+        title="Rate this product"
+        centered
+        closeIcon={false}
+        open={modal2Open}
+        onOk={() => setModal2Open(false)}
+        onCancel={() => setModal2Open(false)}
+      >
+        <Input placeholder="Add comments here" maxLength={150} onChange={onCommentChange} />
+        <label>Rating</label>
+        <Rate style={{ paddingTop: '10px' }} allowHalf defaultValue={2.5} />
+      </Modal>
+    </div>
+  )
+}
+
+export default Rating;

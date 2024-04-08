449460ab7432fe1543efc8597578282c96abab27
ReemaKusa <s12112910@stu.najah.edu>
2024-04-08 13:51:19 +0300

    Website intro

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,20 @@
+import './Intro.css';
+
+const intro =() => {
+
+    return (
+    
+  <>
+  
+     <div class="logo-container">
+         <h1 id="page-logo">Giftopia ...</h1>
+    </div>
+ 
+  </>
+
+  )
+ 
+};
+
+
+export default intro;

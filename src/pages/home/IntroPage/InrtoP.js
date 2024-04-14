5751bef19689858f815572218169d93fea49ed13
ReemaKusa <s12112910@stu.najah.edu>
2024-04-14 19:43:27 +0300

    Website inrto "PreLoader"

---
+++ src/pages/home/IntroPage/InrtoP.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Intro from '../../components/intro/Intro'
+
+
+function IntroP () {
+
+    return (
+      <>
+      <Intro></Intro>
+      </>
+    )
+  }
+  
+
+  
+export default IntroP;

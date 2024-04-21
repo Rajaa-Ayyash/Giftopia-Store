b5d27fff5599c34a0a795be37f539d1879bfb5ec
Muawiya Ismail <s12115192@stu.najah.edu>
2024-04-21 20:05:40 +0300

    creating frontend code for RegistrationFormPage

---
+++ src/pages/registrationFormPage/RegistrationFormPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import RegistrationForm from '../../components/registrationForm/RegistrationForm'
+export default function RegistrationFormPage() {
+  return (
+    <>
+        <RegistrationForm/>
+    </>
+  )
+}

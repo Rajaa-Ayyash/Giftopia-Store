557795cea732974be28be033d4409051037ba8a4
Muawiya Ismail <s12115192@stu.najah.edu>
2024-05-07 19:30:19 +0300

    adding code need to connect to api

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import RegistrationFormPage from "../registrationFormPage/RegistrationFormPage"
 export default function Home() {
   return (
-    <div>Home</div>
+    <div><RegistrationFormPage/></div>
   )
 }

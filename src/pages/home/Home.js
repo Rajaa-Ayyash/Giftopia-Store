efdefa1117895da11e64fe5e6e44ab013ca9f81b
Muawiya Ismail <s12115192@stu.najah.edu>
2024-05-07 19:32:25 +0300

    cleanin home.js file

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import RegistrationFormPage from "../registrationFormPage/RegistrationFormPage"
+
 export default function Home() {
   return (
-    <div><RegistrationFormPage/></div>
+    <div>Home</div>
   )
 }

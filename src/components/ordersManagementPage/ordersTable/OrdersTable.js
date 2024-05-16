97a578a7c3e3f2eb61f68312be885070b40629fd
Muawiya Ismail <s12115192@stu.najah.edu>
2024-05-16 17:46:30 +0300

    merge to main and then adding cookies to categoy management also pushing new package-lock and package

--- src/components/ordersManagementPage/ordersTable/OrdersTable.js
+++ src/components/ordersManagementPage/ordersTable/OrdersTable.js
@@ -1,238 +1,122 @@
-import React, { useState } from 'react';
-import "./OrdersTable.css"
+import React, { useState, useEffect } from 'react';
+import "./CategoriesTable.css"
 import Dropdown from "./dropdown/Dropdown.js";
-import EditModal from "./editModal/EditModal.js";
-import {  FaSearch,FaTrash ,FaExternalLinkAlt,FaTimes } from 'react-icons/fa';
-import { RiEdit2Line } from 'react-icons/ri';
+import AddNewModal from "./addNewModal/AddNewModal.js";
+import {  FaSearch,FaTrash,FaTimes } from 'react-icons/fa';
+import axios from 'axios';
+import Cookie from 'cookie-universal';
 
-
-export default function OrdersTable (){
-  const [rows, setRows]= useState([
-    {
-      orderID: "1",
-      productNumber: "1",
-      productTitle:"Iphone",
-      status: "Processing",
-      price: "99$",
-      customerName: "Cody Fisher",
-      customerEmail:"jane.smith@example.com",
-      location: "Mystic Springs, Arcadia, Wonderland",
-      orderDate: "2,2,2018",
-      dateDelivered: "2,3,2019",
-    },
-    {
-      orderID: "2",
-      productNumber: "2",
-      productTitle:"Ps5",
-      status: "Completed",
-      price: "199$",
-      customerName: "Ali ismail",
-      customerEmail:"test.user@example.com",
-      location: "Whispering Pines, Arcadia, Wonderland",
-      orderDate: "10,8,2020",
-      dateDelivered: "10,9,2020",
-    },
-    {
-      orderID: "3",
-      productNumber: "3",
-      productTitle:"Flash",
-      status: "Shipped",
-      price: "19$",
-      customerName: "Ahmad Fisher",
-      customerEmail:"random.person@example.com",
-      location: "Celestial Harbor, Arcadia, Wonderland",
-      orderDate: "4,5,2020",
-      dateDelivered: "30,5,2020",
-    },
-    {
-      orderID: "4",
-      productNumber: "4",
-      productTitle:"Usp",
-      status: "Refunded",
-      price: "40$",
-      customerName: "Mohammed Ali",
-      customerEmail:"fake.email@example.com",
-      location: "Ember Peak, Arcadia, Wonderland",
-      orderDate: "14,6,2023",
-      dateDelivered: "23,6,2023",
-    },
-    {
-      orderID: "5",
-      productNumber: "5",
-      productTitle:"Laptop",
-      status: "Cancelled",
-      price: "2000$",
-      customerName: "Cody Fisher",
-      customerEmail:"sample.address@example.com",
-      location: "Sapphire Valley, Arcadia, Wonderland",
-      orderDate: "24,6,2021",
-      dateDelivered: "7,7,2021",
-    },
-    {
-      orderID: "5",
-      productNumber: "6",
-      productTitle:"Pc",
-      status: "Failed",
-      price: "123$",
-      customerName: "Cody Fisher",
-      customerEmail:"placeholder.email@example.com",
-      location: "Eternal Glade, Arcadia, Wonderland",
-      orderDate: "15,2,2022",
-      dateDelivered: "23,3,2022",
-    },
-    {
-      orderID: "7",
-      productNumber: "7",
-      productTitle:"Samsung",
-      status: "Processing",
-      price: "37$",
-      customerName: "Cody Fisher",
-      customerEmail:"john.doe@example.com",
-      location: "Mystic Springs, Arcadia, Wonderland",
-      orderDate: "10,4,2018",
-      dateDelivered: "6,5,2018",
-    },
-  ])
-  const [selectedSearchBy,setSelectedSearchBy] = useState("Order ID")
-  const [open,setOpen] = useState(false)
-  const [rowToEditProductNumber, setRowToEditProductNumber] = useState(0);
+export default function CategoriesTable(){
+  const [rows, setRows]= useState([])
+  const [selectedSearchBy,setSelectedSearchBy] = useState("Category Name")
   const [search,setSearch]= useState('')
+  const [openAddNewModal, setOpenAddNewModal] = useState(false);
 
-  function handelStatusDesign (statusType){
-      switch(statusType){
-        case 'Processing':
-          return 'processing-status-design';
-          break;
-        case 'Completed':
-          return 'completed-status-design';
-          break;
-        case 'Shipped':
-          return 'shipped-status-design';
-          break;
-        case 'Refunded':
-          return 'refunded-status-design';
-          break;
-        case 'Cancelled':
-          return 'cancelled-status-design';
-          break;
-        default:
-          return 'failed-status-design';
-      }
-  }
+  const cookie = Cookie();
+  const authToken = cookie.get('GiftopiaToken');
 
-  function handleDeleteRow (targetIndex){
-    setRows(rows.filter(row => row.productNumber !== targetIndex));
-  }
-
-  function handleEditRow(targetIndex){
-    setRowToEditProductNumber(targetIndex);
-    setOpen(true);
-  }
-
-  function handleSubmit (newRow) {
-    rowToEditProductNumber === null
-      ? setRows([...rows, newRow])
-      : setRows(
-          rows.map((currRow) => {
-            if (currRow.productNumber !== rowToEditProductNumber) return currRow;
-            return newRow;
-          })
-        );
-  };
-
-  function handleSearchType(item){
-    switch(selectedSearchBy){
-      case 'Order ID':
-        return search.toLowerCase() === '' ? item : item.orderID.toLowerCase().includes(search);
-        break;
-      case 'Product Number':
-        return search.toLowerCase() === '' ? item : item.productNumber.toLowerCase().includes(search);;
-        break;
-        case 'Product Title':
-        return search.toLowerCase() === '' ? item : item.productTitle.toLowerCase().includes(search);;
-        break;
-      case 'Status':
-        return search.toLowerCase() === '' ? item : item.status.toLowerCase().includes(search);;
-        break;
-      case 'Price':
-        return search.toLowerCase() === '' ? item : item.price.toLowerCase().includes(search);;
-        break;
-      case 'Customer Name':
-        return search.toLowerCase() === '' ? item : item.customerName.toLowerCase().includes(search);;
-        break;
-      case 'Customer Email':
-        return search.toLowerCase() === '' ? item : item.customerEmail.toLowerCase().includes(search);;
-        break;
-      case 'Location':
-        return search.toLowerCase() === '' ? item : item.location.toLowerCase().includes(search);;
-        break;
-      case 'Order Date':
-        return search.toLowerCase() === '' ? item : item.orderDate.toLowerCase().includes(search);;
-        break;
-        case 'Date Delivered':
-          return search.toLowerCase() === '' ? item : item.dateDelivered.toLowerCase().includes(search);;
-          break;
-      default:
-        return search.toLowerCase() === '' ? item : item.orderID.toLowerCase().includes(search);;
+  async function handleDeleteRow (categoryName){
+    try {
+      await axios.delete('http://localhost:6060/category/deleteCategory', {
+        headers: {
+          Authorization: authToken
+        },
+        data:{
+          name:categoryName
+        }
+      });
+      getCategories();
+    } catch (error) {
+      alert(error.response.data.message);
     }
   }
+
+  function handleSearchType(row){
+    return search.toLowerCase() === '' ? row : row.name.toLowerCase().includes(search); 
+  }
+
+  function handleAddNewCategory() {
+    setOpenAddNewModal(true);
+  }
+
+  async function handleAddNewCategorySubmit(categoryName){
+  
+      if(categoryName){
+        try {
+          const response = await axios.post('http://localhost:6060/category/addNewCategory', {
+            name: categoryName
+          }, {
+            headers: {
+              Authorization: authToken
+            }
+          });
+          if(response.data.message !== "category already exists"){
+            getCategories();
+          }
+          else{
+            alert("There is already a category with this name.");
+          }
+        } catch (error) {
+          alert(error.response.data.message);
+        }  
+      }else{
+        alert("Add new category name.");
+      }
+}
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  async function getCategories(){
+    try {
+      const response = await axios.get('http://localhost:6060/category/displayAllCategories', {
+        headers: {
+          Authorization: authToken
+        }
+      });
+      setRows(response.data)
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
 
     return (
       <div className="body">
         <div className="head">
-          <div className="title">Orders</div>
-            <div className="search-field">
-              <Dropdown selected={selectedSearchBy} setSelected={setSelectedSearchBy} />
-              <div className="search-input">
-                <input className="search-input-field" placeholder="Search" value={search} onChange={(e) => (setSearch(e.target.value))} />
-                {search ? <div className='clear-icon' onClick={()=>(setSearch(''))}><FaTimes /></div> : ''}
+          <div className="title">Categories</div>
+            <div className='addNewRow-field-and-search-field'>
+              <div className="addNewRow-field">
+                <button onClick={() => handleAddNewCategory()}>Add New</button>
               </div>
-                <div className='search-icon'><FaSearch /></div>
-            </div>
+              <div className="search-field">
+                <Dropdown selected={selectedSearchBy} setSelected={setSelectedSearchBy} />
+                <div className="search-input">
+                  <input className="search-input-field" placeholder="Search" value={search} onChange={(e) => (setSearch(e.target.value))} />
+                  {search ? <div className='clear-icon' onClick={()=>(setSearch(''))}><FaTimes /></div> : ''}
+                </div>
+                  <div className='search-icon'><FaSearch /></div>
+              </div>
+            </div> 
         </div>
         <div className="table">
           <table className="ordersTable">
             <thead>
               <tr>
-                <th className="columnHeader">ORDER ID</th>
-                <th className="columnHeader">PRODUCT NUMBER</th>
-                <th className="columnHeader">PRODUCT TITLE</th>
-                <th className="columnHeader">STATUS</th>
-                <th className="columnHeader">PRICE</th>
-                <th className="columnHeader">CUSTOMER NAME</th>
-                <th className="columnHeader">CUSTOMER EMAIL</th>
-                <th className="columnHeader">LOCATION</th>
-                <th className="columnHeader">ORDER DATE</th>
-                <th className="columnHeader">DATE DELIVERED</th>
-                <th className="columnHeader">ITEM</th>
-                <th className="columnHeader">EDIT</th>
+                <th className="columnHeader">Category Name</th>
                 <th className="columnHeader">DELETE</th>
               </tr>
             </thead>
             <tbody>
-              {rows.filter((item)=>{
-                return handleSearchType(item);
+              {rows.filter((row)=>{
+                return handleSearchType(row);
               }).map((row) => {
                 return (
-                  <tr key={row.productNumber}>
-                    <td>{row.orderID}</td>
-                    <td>{row.productNumber}</td>
-                    <td>{row.productTitle}</td>
-                    <td><div className={handelStatusDesign(row.status)}>{row.status}</div></td>
-                    <td>{row.price}</td>
-                    <td>{row.customerName}</td>
-                    <td>{row.customerEmail}</td>
-                    <td>{row.location}</td>
-                    <td>{row.orderDate}</td>
-                    <td>{row.dateDelivered}</td>
+                  <tr key={row.name}>
+                    <td>{row.name}</td>
                     <td>
-                      <button className='got-to-item-page-button'><FaExternalLinkAlt/></button>
-                    </td>
-                    <td>
-                      <button className='edit-button' onClick={()=>(handleEditRow(row.productNumber))}><RiEdit2Line/></button>
-                    </td>
-                    <td>
-                      <button className='delete-button' onClick={()=>handleDeleteRow(row.productNumber)}><FaTrash/></button>
+                      <button className='delete-button' onClick={()=>handleDeleteRow(row.name)}><FaTrash/></button>
                     </td>
                   </tr>
                 );
@@ -240,7 +124,7 @@ export default function OrdersTable (){
             </tbody>
           </table>
         </div>
-          { open && <EditModal closeEditModal={()=>(setOpen(false))} defaultValue={rowToEditProductNumber !== null && rows.find(row => row.productNumber === rowToEditProductNumber)} onSubmit={handleSubmit} /> }
+          {openAddNewModal && <AddNewModal closeAddNewModal={() => setOpenAddNewModal(false)} onSubmit={handleAddNewCategorySubmit} />}
       </div>
     );
 }

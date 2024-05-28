f6c8868646e30ec0fea0a7511c2e8eb17d165eab
Muawiya Ismail <s12115192@stu.najah.edu>
2024-05-28 13:53:16 +0300

    pushing new dashboard code

--- src/components/adminDashboard/AdminDashboard.js
+++ src/components/adminDashboard/AdminDashboard.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import "./AdminDashboard.css"
+import {useNavigate } from 'react-router-dom'
 export default function AdminDashboard() {
+    const navigate = useNavigate();
   return (
     <div className='dashboard-body'>
         <div className='dashboard-navigation'>
-            <div className='dashboard-forms'>
-                <div className='dashboard-forms-first-row'>
-                    <form action='/admins' method='get'>
-                        <input type='submit' className='dashboard-admins-list-input' value='Admins'/>
-                    </form>
-                    <form action='/users' method='get'>
-                        <input type='submit' className='dashboard-users-list-input' value='Users'/>
-                    </form>
-                </div>
-                <div className='dashboard-forms-second-row'>
-                    <form action='/products' method='get'>
-                        <input type='submit' className='dashboard-products-input' value='Products'/>
-                    </form>
-                    <form action='/catagories' method='get'> 
-                        <input type='submit' className='dashboard-catagories-input' value='Catagories'/>
-                    </form>
-                </div>
+            <div className='dashboard-navigation-buttons'>
+                    <div className='dashboard-first-row-of-buttons'>
+                        <button  onClick={() => navigate('/admins')} className='dashboard-admins-list-button'>Admins</button>
+                        <button  onClick={() => navigate('/users')} className='dashboard-users-list-button'>Users</button>
+                    </div>
+                    <div className='dashboard-second-row-of-buttons'>
+                        <button  onClick={() => navigate('/products')} className='dashboard-products-list-button'>Products</button>
+                        <button  onClick={() => navigate('/catagories')} className='dashboard-catagories-list-button'>Catagories</button>
+                    </div>
             </div>
         </div>
     </div>
